c5f5aa5c943b5ef5987d9f292796c05eab5155c0
Josef Perry Tinagan <josephperry_06@yahoo.com>
2016-10-05 23:17:26 +0800

    added functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
+
 	def index
 		@articles = Article.all
 	end
@@ -7,4 +9,40 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 		
 	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+  		@article = Article.new(article_params)
+  		@article.save
+
+  		flash.notice = "Article '#{@article.title}' created!"
+
+  		redirect_to article_path(@article)
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		flash.notice = "Article '#{@article.title}' updated!"
+
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@desarticle = Article.find(params[:id])
+		@desarticle.delete
+
+		flash.notice = "Article '#{@desarticle.title}' deleted!"
+
+		redirect_to articles_path
+	end
+
 end
